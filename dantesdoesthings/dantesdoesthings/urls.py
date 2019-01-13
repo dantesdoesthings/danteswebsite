4695f708a813112def37119656ce3804852d96ab
dantesdoesthings <dantesdoesthings@gmail.com>
2019-01-13 09:28:43 -0500

    Index app additions
    1. Basic modifications and trying out different things.

--- dantesdoesthings/dantesdoesthings/urls.py
+++ dantesdoesthings/dantesdoesthings/urls.py
@@ -1,8 +1,11 @@
 """dantesdoesthings URL Configuration"""
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
+    path('', include('index.urls')),
+    path('index/', include('index.urls')),
     path('admin/', admin.site.urls),
 ]
