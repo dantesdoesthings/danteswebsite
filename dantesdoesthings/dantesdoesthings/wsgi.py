f478760a79277abb5e9c3fb385be40a761d37348
dantesdoesthings <dantesdoesthings@gmail.com>
2019-01-12 19:15:47 -0500

    Cleanup
    1. Removed extraneous comments and docstrings generated by Django.

--- dantesdoesthings/dantesdoesthings/wsgi.py
+++ dantesdoesthings/dantesdoesthings/wsgi.py
@@ -1,11 +1,4 @@
-"""
-WSGI config for dantesdoesthings project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
+"""WSGI config for dantesdoesthings project."""
 
 import os
 
