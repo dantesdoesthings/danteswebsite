0c19fd731dbcac797d50499afe72e9e679e78e31
dantesdoesthings <dantesdoesthings@gmail.com>
2019-01-14 21:46:41 -0500

    Data Generator
    1. Added an html file for the dantes_data_generator app.
    2. Added this app to the navbar and added appropriate connections.

--- dantesdoesthings/dantes_data_generator/views.py
+++ dantesdoesthings/dantes_data_generator/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def data_generator(request):
+    return render(request, 'dantes_data_generator/data_generator.html')
--- dantesdoesthings/dantesdoesthings/urls.py
+++ dantesdoesthings/dantesdoesthings/urls.py
@@ -7,5 +7,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('dantes_site.urls')),
     path('index/', include('dantes_site.urls')),
+    path('data_generator/', include('dantes_data_generator.urls')),
     path('admin/', admin.site.urls),
 ]
