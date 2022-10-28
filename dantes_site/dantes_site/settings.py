036b0adc3db194bfa5589580cb9856b6267b7bde
dantesdoesthings <PrinceL2018>
2022-10-28 12:02:59 -0400

    Static files
    1. Fixing issue with collectstatic

--- dantes_site/dantes_site/settings.py
+++ dantes_site/dantes_site/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+    BASE_DIR / 'dantes_data_generator' / 'static',
+    BASE_DIR / 'site_base' / 'static',
+    BASE_DIR / 'users' / 'static',
 ]
 STATIC_ROOT = BASE_DIR.parent/'static'
 
